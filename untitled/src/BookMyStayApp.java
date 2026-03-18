6c0ce67ba880945522630e6ec76c8b23d7945ed7
yk5287 <yk5287@srmist.edu.in>
2026-03-18 11:10:24 +0530

    AADITH Added feature/UC1-WelcomePage

---
+++ untitled/src/BookMyStayApp.java
@@ -0,0 +1,10 @@
+public class BookMyStayApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println("      Welcome to BookMyStay      ");
+        System.out.println("=================================");
+    }
+
+}
